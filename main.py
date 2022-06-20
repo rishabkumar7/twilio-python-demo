1da6d8de562e8c4c4cdcfc892af5c767d83345d9
Rishab Kumar <rishabkumar7@gmail.com>
2022-06-20 16:48:17 -0400

    modified the response body

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+#Importing the modules
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
 
 
 app = Flask(__name__)
 
+#Post Request Webhook
 @app.route('/sms', methods=['POST'])
 
-def sms():
-  number = request.form['From']
-  message_body = request.form['Body']
-  resp = MessagingResponse()
-  resp.message('Hello {}, you said: {}'.format(number, message_body))
-  return str(resp)
+#Defining the function
 
+def sms():
+  resp = MessagingResponse()
+  resp.message('Hello, welcome to Twilio Masterclass, your host Rishab here. I have got a gift for you, Twilio Startup credits --> https://twiliostartups.com/collision')
+  return str(resp), 200
+
+# Running the Flask web server
 if __name__ == '__main__':
     app.run()
